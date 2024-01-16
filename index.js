0a1f0ec08a8cbffb149dfc070d2bc9193fa61e8e
David <cheruiyotscripts@gmail.com>
2024-01-16 13:41:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word===word.split('').reverse().join('');
+  //   return "true";
+  // }else{
+  //   return "false";
+  // }
+  
 }
+
 
 /* 
   Add your pseudocode here
 */
+//write function that will take string as Parameter
+//within function have a condition checking if string is palindrome
+//if string is palindrome return  true else return false
 
 /*
   Add written explanation of your solution here
-*/
+*/ //from the function
+//.split() breaks the string into an array 
+//.reverse() will reverse the broken array
+//.join() will join the array into a string
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
